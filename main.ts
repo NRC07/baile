6cc7b1a88a744f3d1fea714ed3ac34d3c882fbbd
NRC07 <99951211+NRC07@users.noreply.github.com>
2022-05-23 13:12:42 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 25)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 14)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 14)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 25)
 })
 input.onButtonPressed(Button.B, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 49)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 8)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 49)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 8)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 49)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 8)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 49)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 8)
 })
 basic.forever(function () {
 	
